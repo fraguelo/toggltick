e84869a3343f758ec6fecb5795a5dd9b08a72be1
fraguelo <fredy.guevara@moove-it.com>
2020-04-24 23:55:42 -0500

    First commit | Basic functionality

---
+++ readme.md
@@ -0,0 +1,30 @@
+# TogglTick
+
+## Configure conf.json
+
+- Toggl: https://toggl.com/app/profile
+- Tick: Tick > Account > Edit Profile
+ *__USER_AGENT__ [email] should be replaced for any email formatted thing*
+
+## To run
+
+```shell
+npm start
+```
+
+## Samples
+
+
+*`Insert a date in any format (dd, dd-mm, dd-mm-yyyy) add '+' for amount of days (dd+5)`*
+```shell
+
+22
+
+22-04
+
+22-04-2020
+
+22+4 (22,23,24,25)
+
+22-04+5
+```
---
+++ src/index.js
@@ -0,0 +1,90 @@
+const fs = require("fs");
+const TogglClient = require("toggl-api");
+const Tickspot = require("tickspotv2-api");
+const prompts = require("prompts");
+
+const {
+  TOGGL_API_TOKEN,
+  TICK_USER_AGENT,
+  TICK_SUBSCRIPTION_ID,
+  TICK_API_TOKEN,
+} = JSON.parse(fs.readFileSync("conf.json"));
+
+const toggl = new TogglClient({ apiToken: TOGGL_API_TOKEN });
+const tick = new Tickspot(
+  TICK_USER_AGENT,
+  TICK_SUBSCRIPTION_ID,
+  TICK_API_TOKEN
+);
+
+let tasks = [];
+
+function buildDate(date) {
+  try {
+    let amountDays = 1;
+    if (date.includes("+")) {
+      amountDays = Number(date.split("+")[1]);
+      date = date.split("+")[0];
+    }
+    const spDate = date.split("-");
+    const currentDate = new Date();
+    const day = Number(spDate[0]);
+    const month = spDate[1] ? Number(spDate[1]) - 1 : currentDate.getMonth();
+    const year = spDate[2] || currentDate.getFullYear();
+    const startDate = new Date(Date.UTC(year, month, day));
+    const endDate = new Date(Date.UTC(year, month, day + amountDays));
+
+    return { startDate, endDate };
+  } catch (error) {
+    console.error("Check the date format!");
+  }
+}
+
+async function getTickTasks() {
+  return (await tick.getTasks()).map(({ id, name }) => ({
+    id,
+    name,
+  }));
+}
+
+function getTaskId(taskName) {
+  return tasks.find(({ name }) => name === taskName).id;
+}
+
+function getTimeEntries({ startDate, endDate }) {
+  toggl.getTimeEntries(startDate, endDate, (err, rawEntries) => {
+    rawEntries.forEach(async ({ start, duration, description, tags }) => {
+      const entry = {
+        task_id: getTaskId(tags[0]),
+        notes: description,
+        name: tags[0],
+        hours: duration / 3600,
+        date: start,
+      };
+
+      const { date, notes, hours } = await tick.createEntry(entry);
+      console.log("saved :>> ", { date, notes, hours });
+    });
+  });
+}
+
+async function upload({ startDate, endDate }) {
+  try {
+    tasks = await getTickTasks();
+    getTimeEntries({ startDate, endDate });
+  } catch (error) {
+    console.log("error :>> ", error);
+  }
+}
+
+async function start() {
+  const response = await prompts({
+    type: "text",
+    name: "date",
+    message:
+      "Insert a date in any format (dd, dd-mm,  dd-mm-yyyy) add '+' for amount of days (dd+5)",
+  });
+  upload(buildDate(response.date));
+}
+
+start();
